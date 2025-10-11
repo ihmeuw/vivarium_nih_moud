522aa49845db27b38a560eb0f645f46e61b5647e
Abraham Flaxman <abie@alum.mit.edu>
2025-10-11 16:53:39 -0700

    Format data utilities and fix relative import

--- src/vivarium_nih_moud/data/utils.py
+++ src/vivarium_nih_moud/data/utils.py
@@ -1,21 +1,27 @@
-import numpy as np, pandas as pd
+import numpy as np
+import pandas as pd
+
 
 def generate_constant_data(data_value):
     ages = np.linspace(0, 100, 11, endpoint=True)
     years = [2021]
-    sexes = ['Male', 'Female']
+    sexes = ["Male", "Female"]
     data = []
     for age in ages:
         for year in years:
             for sex in sexes:
-                data.append({
-                    'age_start': age,
-                    'age_end': age+10,
-                    'year_start': year,
-                    'year_end': year+1,
-                    'sex': sex,
-                })
+                data.append(
+                    {
+                        "age_start": age,
+                        "age_end": age + 10,
+                        "year_start": year,
+                        "year_end": year + 1,
+                        "sex": sex,
+                    }
+                )
                 for i in range(1_000):
-                    data[-1][f'draw_{i}'] = np.clip(data_value+np.random.uniform(0,.1), 0, 1)
+                    data[-1][f"draw_{i}"] = np.clip(
+                        data_value + np.random.uniform(0, 0.1), 0, 1
+                    )
     data = pd.DataFrame(data)
-    return data.set_index(['sex', 'age_start', 'age_end', 'year_start', 'year_end'])
+    return data.set_index(["sex", "age_start", "age_end", "year_start", "year_end"])
